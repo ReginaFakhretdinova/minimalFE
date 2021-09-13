bfc661065335263e200ea4308bc2fd8c66ab6600
ReginaFakhretdinova <85935108+ReginaFakhretdinova@users.noreply.github.com>
2021-09-13 12:46:13 +0300

    Add files via upload

---
+++ src/mesh/fusmesh.py
@@ -0,0 +1,35 @@
+#
+# This file is part of minimalFE.
+#
+# Created by Brice Lecampion on 07.02.21.
+# Copyright (c) ECOLE POLYTECHNIQUE FEDERALE DE LAUSANNE, Switzerland, Geo-Energy Laboratory, 2016-2021.  All rights reserved.
+# See the LICENSE.TXT file for more details. 
+#
+
+import numpy as np
+
+# Unstructured mesh class - minimal
+# dependencies: meshio package (pip install meshio)
+
+class fusmesh :
+    """" A basic and minimal mesh class """
+
+    def __init__(self,dimension,coor,conn,order):
+        assert type(dimension) is int, "dimension is not an integer "
+        assert type(coor) is np.ndarray, "given coordinates is not a numpy array"
+        assert type(conn) is list, "given connectivity is not a numpy array "
+        #assert conn.dtype == "int", "given connectivity is not made of integer" #change to np.array
+
+        self.dimension = dimension
+        self.coor = coor
+        self.conn = conn
+        self.order = order
+        self.nelts = conn.shape[0]
+        self.nnodes = coor.shape[0]
+
+    @classmethod
+    def fromMeshio(cls,meshio,order):
+        # for now we only accept mesh made  of a single type of element
+        lk=list(meshio.cells_dict.keys())
+        assert len(lk)==1
+        return cls(meshio.points.shape[1],meshio.points,meshio.cells_dict[lk[0]],order)
